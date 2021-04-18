ed05af15a195e2a83bae45d18ad91bfb74aad22e
ChiChi_STAT <yzhan239@ncsu.edu>
2021-04-18 09:06:15 -0400

    update

--- oldversion_w_bspline/NP_Causal_functions.R
+++ oldversion_w_bspline/NP_Causal_functions.R
@@ -11,7 +11,6 @@ library(magrittr)
 library(gam)
 library(expm)
 library(orthopolynom)
-library(MASS)
 library(polynom)
 #Auxiliary Functions
 #Smoothing splines 
@@ -55,32 +54,35 @@ CV_knn <- function(j,k_pool,x,y){
   return(k_pool[which(error_k == min(error_k))][1])
 }
 ##Basis construction
+Bspline_construct <- function(X ,df, degree, range){
+  knots <- seq(range[1], range[2], (range[2] - range[1])/(df - degree))
+  knots <- knots[-1]; knots <- knots[1:(df - degree-1)] 
+  return(bSpline(X, knots = knots, degree = degree, intercept = TRUE, Boundary.knots = range))
+}
 gram_schmidt <- function(polylist,range){
-  len <- length(polylist)
-  inner_prod <- function(poly1, poly2,range){
-    polynom::integral(poly1*poly2, limits = range) 
+  len <- max(length(polylist),3)
+  inner_prod <- function(poly1, poly2){
+    integral(poly1*poly2, limits = range) 
   }
   #first basis
   othno_poly <- list()
-  othno_poly[[1]] <- polylist[[1]]/sqrt(inner_prod(polylist[[1]],polylist[[1]],range))
+  othno_poly[[1]] <- polylist[[1]]/sqrt(inner_prod(polylist[[1]],polylist[[1]]))
   #second basis
-  unnorm_ver <- polylist[[2]] - inner_prod(polylist[[2]],othno_poly[[1]],range) * othno_poly[[1]]
-  othno_poly[[2]] <- unnorm_ver/sqrt(inner_prod(unnorm_ver,unnorm_ver,range))
+  unnorm_ver <- polylist[[2]] - inner_prod(polylist[[2]],othno_poly[[1]]) * othno_poly[[1]]
+  othno_poly[[2]] <- unnorm_ver/sqrt(inner_prod(unnorm_ver,unnorm_ver))
   for (i in 3:len){
-    unnorm_ver <- othno_poly[[i-1]]*polynomial(c(0,1)) - inner_prod(othno_poly[[i-2]],othno_poly[[i-1]]*polynomial(c(0,1)),range)*othno_poly[[i-2]] 
-    - inner_prod(othno_poly[[i-1]],othno_poly[[i-1]]*polynomial(c(0,1)),range)*othno_poly[[i-1]]
-    othno_poly[[i]] <- unnorm_ver/sqrt(inner_prod(unnorm_ver,unnorm_ver,range))
+    unnorm_ver <- othno_poly[[i-1]]*polynomial(c(0,1)) - inner_prod(othno_poly[[i-2]],othno_poly[[i-1]]*polynomial(c(0,1)))*othno_poly[[i-2]] 
+    - inner_prod(othno_poly[[i-1]],othno_poly[[i-1]]*polynomial(c(0,1)))*othno_poly[[i-1]]
+    othno_poly[[i]] <- unnorm_ver/sqrt(inner_prod(unnorm_ver,unnorm_ver))
   }
   othno_poly
 }
 Powerseries_construct_T <- function(X ,df, range){
-  len <- max(3,df)
   polylist <- list()
-  for (i in 1:len){
+  for (i in 1:df){
   polylist[[i]] <-  polynomial(c(0,rep(0,i - 1), 1))
   }
-  othno_poly <- gram_schmidt(polylist, range)[1:df]
-  M <- matrix(0, nrow = length(X), ncol = df)
+  othno_poly <- gram_schmidt(polylist, range[[1]])
   for (i in 1:length(X)){
     for (j in 1:df){
       M[i,j] <- as.function(othno_poly[[j]])(X[i])
@@ -89,13 +91,11 @@ Powerseries_construct_T <- function(X ,df, range){
   return(M)
 }
 Powerseries_construct_V = function(X ,df, range){
-  len <- max(3,df)
   polylist <- list()
-  for (i in 1:len){
+  for (i in 1:df){
     polylist[[i]] <-  polynomial(c(rep(0,i - 1), 1))
   }
-  othno_poly <- gram_schmidt(polylist, range)[1:df]
-  M <- matrix(0, nrow = length(X), ncol = df)
+  othno_poly <- gram_schmidt(polylist, range[[1]])
   for (i in 1:length(X)){
     for (j in 1:df){
       M[i,j] <- as.function(othno_poly[[j]])(X[i])
@@ -110,17 +110,28 @@ rowwise_kron <- function(M1,M2){
   }
   return(M)
 }
-Basis_M_Build <- function(V, Tt, Vbasis_name, Tbasis_name, df_V, df_T, Vrange_list, Trange_list){
+Basis_M_Build <- function(V, Tt, Vbasis_name, Tbasis_name, df_V, df_T, degree, Vrange, Trange){
   n <- length(Tt); dim_V <- ncol(V)
   basis_V <-  case_when(
+    Vbasis_name == 'Bspline' ~ {
+      if (Vbasis_name == 'Bspline' ){
+      apply(V, MARGIN = 2, FUN = function(v){Bspline_construct(v, df_V, degree, Vrange)})
+      } else {0}
+    },
     Vbasis_name == 'PowerSeries' ~ {
-      lapply(1:ncol(V), function(i){Powerseries_construct_V(X = V[,i], df = df_V[i],range = Vrange_list[[i]])})
+      apply(V, MARGIN = 2, FUN = function(v){Powerseries_construct_V(v, df_V, Vrange)})
     }
-  ) 
+  ) %>% 
+    as.data.frame %>% as.list %>%
+    lapply(function(v){v %>% as.vector %>% matrix(nrow = n)})
   
   basis_T <- case_when(
+    Tbasis_name == 'Bspline' ~ {
+      # apply(Tt, MARGIN = 2, FUN = function(v){Bspline_construct(v, df_T, degree, Trange)})
+      0
+    },
     Tbasis_name == 'PowerSeries' ~ {
-      Powerseries_construct_T(Tt[,1], df_T[1], Trange_list[[1]])
+      apply(Tt, MARGIN = 2, FUN = function(v){Powerseries_construct_T(v, df_T, Trange)})
     }
   ) %>% 
     as.data.frame %>% as.list %>%
@@ -137,35 +148,30 @@ Basis_M_Build <- function(V, Tt, Vbasis_name, Tbasis_name, df_V, df_T, Vrange_li
 
 #Main Functions
 GCCTE_fit = function(X, idxV = c(1), Tt, Y, Vbasis_name ='PowerSeries', Tbasis_name = 'PowerSeries',
-                     df_V_pool_list, df_T_pool_list, degree, q_type = 'optimal_nodomain',
-                     mu0, ET_X, Vrange_list, Trange_list, k_pool, J = 7, 
-                     nusi_est = 'SS', CI = FALSE, up_spnum){
+                     df_V_pool, df_T_pool, degree, q_type = 'optimal', mu0, ET_X, Vrange, Trange, k_pool, J = 7, 
+                     nusi_est = 'SS', CI = FALSE){
 #variable description
 ##X: n by dx matrix; idxV: coordinates in X that are V; Tt: treatment vector; Y: outcome vector
 ##Vbasis: basis type for V; Tbasis:basis type for T
-##df_V_pool_list, df_T_pool_list: list of degrees of V/T that one has interest to test with GCV 
+##df_V_pool, df_T_pool: degrees of V that one has interest to test with GCV 
 ##degree: degree of the Bspline
 ##Kn_max: max number of Kn for GCV
 ##q_type: type of q function
 ##mu0, ET_X: estimated nusicance vectors
-##Vrange_list, Trange_list: lists for range of V and T
+##Vrange, Trange: lists for range of V and T
 ##k_pool: candidates of k for knn regression
 ##J: number of rounds for knn's CV
 ##nusi_est: estimator for nusicance, 1) 'SS' = smoothness spline with default settings in  GAM package; 2) 'KNN' = k-nearest neighborhood regression
 ##CI: TRUE if want to get confidence interval in est_tau
-##upper bound of splines
+  
 #solve function to fit with certian degree of V and T
   solve_GCCTE = function(df_V, df_T, CI = FALSE){
-    ind_nonzero <- which(df_V != 0)
-    if (length(ind_nonzero) == 0){return(list(singular = 1))}
-    Kn = prod(df_V[ind_nonzero]) * df_T
-    if (Kn > up_spnum){return(list(singular = 1))}
+    Kn = df_V * df_T
     #build basis
-    basis_M <- Basis_M_Build(V = (V %>% as.matrix())[,ind_nonzero] %>% as.matrix(), Tt, Vbasis_name, Tbasis_name, 
-                             df_V[ind_nonzero], df_T, Vrange_list[ind_nonzero], Trange_list)
+    basis_M <- Basis_M_Build(V, Tt, Vbasis_name, Tbasis_name, df_V, df_T, degree, Vrange, Trange)
     #build Q matrix of q function
     Q <- case_when( 
-      q_type == 'optimal_nodomain' ~ {
+      q_type == 'optimal' ~ {
         q_train <- apply(basis_M, MARGIN = 2, function(v){(ET_X - Tt)*v}) %>%
           as.data.frame %>% as.list;
         if (nusi_est == 'KNN'){
@@ -184,7 +190,7 @@ GCCTE_fit = function(X, idxV = c(1), Tt, Y, Vbasis_name ='PowerSeries', Tbasis_n
     ) %>% matrix(nrow = n)
     #Get Smoothing matrix S for GCV and Phi for tau estimation
     QKn <- Q[, 1:Kn]
-    MpartI <- apply(QKn, 2, function(v){v*(Tt - ET_X)}) %>% t %*% basis_M[, 1:Kn] %>% ginv
+    MpartI <- apply(QKn, 2, function(v){v*(Tt - ET_X)}) %>% t %*% basis_M[, 1:Kn] %>% solve 
     MpartII <- QKn %>% t %*% diag(as.numeric(Tt - ET_X))
     MpartIII <- basis_M[, 1:Kn] 
     phi <- MpartI %*% MpartII %*% (Y - mu0)
@@ -202,10 +208,10 @@ GCCTE_fit = function(X, idxV = c(1), Tt, Y, Vbasis_name ='PowerSeries', Tbasis_n
       }
       G = G/n
       B = B/n
-      pre_A = 1/sqrt(Kn) * ginv(G) %*% expm::sqrtm(B)
-      return(list(phi = phi, S = S, MSE = MSE, pre_A = pre_A, singular = 0))
+      pre_A = 1/sqrt(Kn) * solve(G) %*% sqrtm(B)
+      return(list(phi = phi, S = S, MSE = MSE, pre_A = pre_A))
     }else{
-    return(list(phi = phi, S = S, MSE = MSE, singular = 0))
+    return(list(phi = phi, S = S, MSE = MSE))
     }
   }
 #pre-setting 
@@ -214,44 +220,30 @@ GCCTE_fit = function(X, idxV = c(1), Tt, Y, Vbasis_name ='PowerSeries', Tbasis_n
   dim_V <- length(idxV)
   n <- length(Y)
 #GCV df selection
-  error_GCV = matrix(0, nrow = lapply(df_V_pool_list,length) %>% unlist %>% prod, ncol = length(df_T_pool_list[[1]]))
-  df_V_pool_grid <- expand.grid(df_V_pool_list) %>% as.matrix %>% t
-  df_V_pool_grid <- split(df_V_pool_grid, rep(1:ncol(df_V_pool_grid), each = nrow(df_V_pool_grid)))
-  count_v <- 0; count_t <- 0;
-  for (df_V in df_V_pool_grid){
-    count_v <- count_v + 1
-    for (df_T in df_T_pool_list[[1]]){
-      count_t <- count_t + 1
+  error_GCV = matrix(0, nrow = length(df_V_pool), ncol = length(df_T_pool))
+  for (df_V in df_V_pool){
+    for (df_T in df_T_pool){
       solve_GCV <- solve_GCCTE(df_V, df_T, FALSE)
-      if (solve_GCV$singular == 1){error_GCV[count_v, count_t] <- +Inf}else{
       S <- solve_GCV$S; phi = solve_GCV$phi 
-      error_GCV[count_v, count_t] <- solve_GCV$MSE/((1 - sum(diag(S))/n)^2)
-      }
+      error_GCV[which(df_V_pool == df_V), which(df_T_pool == df_T)] <- solve_GCV$MSE/((1 - sum(diag(S))/n)^2)
     }
-    count_t <- 0
   }
-  df_V_opt <- df_V_pool_grid[[which(error_GCV == min(error_GCV), arr.ind = TRUE)[1,1]]]
-  df_T_opt <- df_T_pool_list[[1]][which(error_GCV == min(error_GCV), arr.ind = TRUE)[1,2]]
+  df_V_opt <- df_V_pool[which(error_GCV == min(error_GCV), arr.ind = TRUE)[1,1]]
+  df_T_opt <- df_T_pool[which(error_GCV == min(error_GCV), arr.ind = TRUE)[1,2]]
 #fitting
   final_solve <- solve_GCCTE(df_V_opt, df_T_opt, TRUE)
   final_model <- list(para = final_solve$phi, df_V = df_V_opt, df_T = df_T_opt, Vbasis_name = Vbasis_name,
-                      Tbasis_name = Tbasis_name, Vrange_list = Vrange_list, Trange_list = Trange_list, 
-                      pre_A = final_solve$pre_A, n = n)
+                      Tbasis_name = Tbasis_name, degree = degree, Vrange = Vrange, Trange = Trange, pre_A = final_solve$pre_A)
   return(final_model)
 }
 
 est_tau <- function(V_pred, Tt_pred, model, CI = FALSE, rate = 0.9){
-  n <- model$n
-  df_est <- model$df_V
-  V_pred_est <- V_pred[,which(df_est != 0)] %>% as.matrix
-  Vrange_list_est <- model$Vrange_list[which(df_est != 0)]
-  df_est <- df_est[which(df_est != 0)]
-  basis_M_pred <- Basis_M_Build(V_pred_est, Tt_pred, model$Vbasis_name, model$Tbasis_name, df_est, 
-                                model$df_T, Vrange_list_est, model$Trange_list)
+  basis_M_pred <- Basis_M_Build(V_pred, Tt_pred, model$Vbasis_name, model$Tbasis_name, model$df_V, 
+                                model$df_T, model$degree, model$Vrange, model$Trange)
   tau = basis_M_pred %*% model$para
   z_value = qnorm(1 - (1 - rate)/2)
   if (CI){
-    Kn <- prod(df_est) * model$df_T
+    Kn <- model$df_V * model$df_T
     upper <- rep(0,length(Tt_pred)); lower <- rep(0,length(Tt_pred))
     for (obj in 1:length(Tt_pred)){
       A <- basis_M_pred[obj,] %*% model$pre_A
@@ -259,9 +251,9 @@ est_tau <- function(V_pred, Tt_pred, model, CI = FALSE, rate = 0.9){
       upper[obj] <- tau[obj] + z_value * sqrt(est_gamma * (Kn) / n)
       lower[obj] <- tau[obj] - z_value * sqrt(est_gamma * (Kn) / n)
     }
-    return(list(tau = tau %>% Re, upper = upper %>% Re, lower = lower %>% Re))
+    return(list(tau = tau, upper = upper, lower = lower))
   }else{
-  return(list(tau = tau %>% Re))
+  return(list(tau = tau))
   }
 }
 
